74734fe9700ac74e4d49336368e0dc5c292ce0bd
shusuke919 <sh091965@gmail.com>
2022-05-17 21:00:55 +0900

    保存

---
+++ form_post.php
@@ -0,0 +1,8 @@
+<?php
+
+$id = $_POST["id"];
+$pass = $_POST["pass"];
+echo  $id. "<br>";
+echo  $pass. "<br>";
+
+?>
---
+++ main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<form method ="get" action="form_get.php" >
+<P>お名前；<input type="text" name="name" size="20" ></P>
+<P>MAIL；<input type="text" name="mail" size="20" ></P>
+<p><input type="submit" value="送信"></p>
+
+<form method ="post" action="form_post.php" >
+<P>ID；<input type="text" name="id" size="20" ></P>
+<P>PASS；<input type="text" name="pass" size="20" ></P>
+<p><input type="submit" value="送信"></p>
+</form>
+
+
+
+
+</form>
+
+  
+</body>
+</html>
