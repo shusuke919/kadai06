4514c5d960c83d6c7c9d8c7b3cfed36eb843368d
shusuke919 <sh091965@gmail.com>
2022-05-20 00:48:02 +0900

    DBと接続ができない

--- form_get.php
+++ form_get.php
@@ -1,21 +1,49 @@
 <?php
 
+// 入力チェック（受信確認処理追加）
+if(
+  !isset($_GET["name"]) || $_GET["name"]=="" ||
+  !isset($_GET["email"]) || $_GET["email"]=="" ||
+  !isset($_GET["naiyou"]) || $_GET["naiyou"]==""
+){
+  exit('ParamError');
+}
+
+// getデータ取得
 $name = $_GET["name"];
-$mail = $_GET["mail"];
-echo  $name. "<br>";
-echo  $mail. "<br>";
-?>
+$email = $_GET["email"];
+$naiyou = $_GET["naiyou"];
+// echo  $name. "<br>";
+// echo  $mail. "<br>";
+
+// DB接続します（エラー処理追加）コピペして部分的に書き換えて使うと良い！
+try {
+  $pdo = new PDO('mysql:dbname=kadai_db;charset=utf8;host=localhost:3306','root','root');
+} catch (PDOException $e) {
+  exit('DbConnectError:'.$e->getMessage());
+}
+
+// データ登録　SQL作成
+$stmt = "INSERT INTO kadai_an_db(id, name, email, naiyou,
+indate )VALUES(NULL, :a1, :a2, :a3, sysdate())";
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':a1', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a2', $email, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a3', $naiyou, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+// データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("QueryError:".$error[2]);
+
+}else{
+  //５．index.phpへリダイレクト
+  header("Location: main.pdp");
+  exit;
+
+}
+?> 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <p><?= $</p>
-</body>
-</html>
--- main.php
+++ main.php
@@ -1,28 +1,69 @@
+<?php
+
+
+
+$d = date("s");
+if ($d >=40 ){
+  echo '<p style="background:red;">40秒以上</p>';
+}else if ($d >=20 ){
+  echo '<p style="background:blue;">1秒以上</p>';
+}else{
+  echo '<p style="background:yellow;">20秒以上</p>';
+}
+echo '<p>現在：'.$d.'秒</p>';
+
+?>
+
+
+
+
+<!-- 　チェックボックス
+<div>
+    <input type="checkbox" name="food[]" value="寿司"> 寿司　
+    <input type="checkbox" name="food[]" value="天ぷら"> 天ぷら　
+    </div>
+
+セレクトボックス -->
+
+<!-- <div>
+<select name= "fruits">
+      <option value = "りんご">りんご</option>
+      <option value = "れもん">れもん</option>
+      <option value = "メロン">メロン</option>
+    </select>
+    </div> -->
+
+
+
+
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>データ登録</title>
+  <style>div{padding: 10px;font-size:16px;}</style>
 </head>
 <body>
-<form method ="get" action="form_get.php" >
-<P>お名前；<input type="text" name="name" size="20" ></P>
-<P>MAIL；<input type="text" name="mail" size="20" ></P>
-<p><input type="submit" value="送信"></p>
-
-<form method ="post" action="form_post.php" >
-<P>ID；<input type="text" name="id" size="20" ></P>
-<P>PASS；<input type="text" name="pass" size="20" ></P>
-<p><input type="submit" value="送信"></p>
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+<!-- Main[Start] -->
+<form method="get" action="form_get.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>フリーアンケート</legend>
+     <label>名前：<input type="text" name="name"></label><br>
+     <label>Email：<input type="text" name="email"></label><br>
+     <label><textArea name="naiyou" rows="4" cols="40"></textArea></label><br>
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
 </form>
-
-
-
-
-</form>
-
-  
-</body>
-</html>
